ec2cf9a212df3c13064e7b683fe9ef2ef4464511
Veaceslav Negru <vnegru@endava.com>
2018-07-30 23:33:22 +0300

    commit first changes

---
+++ LearnAutomation/LearnAutomation/Executor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Automation
+{
+    public class Executor
+    {
+        public IWebDriver driver;
+
+        public Executor()
+        {
+        }
+
+        public Executor(String browser)
+        {
+
+            switch (browser.ToLower())
+            {
+                case "chrome":
+                    driver = new ChromeDriver();
+                    driver.Manage().Window.Maximize();
+                    Console.WriteLine(browser + " browser instance started.");
+                    break;
+                case "firefox":
+                    Console.WriteLine("Not implemented for " + browser);
+                    break;
+                case "ie":
+                    Console.WriteLine("Not implemented for " + browser);
+                    break;
+                default:
+                    Console.WriteLine("Not implemented for " + browser);
+                    break;
+            }
+
+        }
+
+        public void NavigateTo(String url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void InputText(String textValue, String findInputByName)
+        {
+            IWebElement element = driver.FindElement(By.Name(findInputByName));
+
+            element.SendKeys(textValue);         }
+
+        public void ClickOnElement(IWebElement element)
+        {
+            try
+            {
+                element.Click(); 
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Web element not found. " + e.Message);
+            }
+        }
+
+        public void FindElementByXPath(String xPath)
+        {
+            ReadOnlyCollection<IWebElement> webElements = driver.FindElements(By.XPath(xPath));
+            int index = 1;
+
+            foreach (IWebElement item in webElements)
+            {
+                Console.WriteLine(index + ": " + item.Text);
+                index++;
+            }
+
+            //return webElements;
+        }
+
+        public IWebElement FindElementByName(String name)
+        {
+            IWebElement webElement = driver.FindElement(By.Name(name));
+            return webElement;
+        }
+
+        public void CountResults(String textValue)
+        {
+            ReadOnlyCollection<IWebElement> resultsList = driver.FindElements(By.PartialLinkText(textValue));
+             int index = 1;              foreach (IWebElement item in resultsList)             {                 Console.WriteLine(index + ": " + item.Text);                 index++;             }
+            Console.WriteLine($"On page {resultsList.Count} search mathing keyword results.");
+        }
+
+        public void Close()
+        {
+            driver.Close();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Automation
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            String url = "http://www.google.com";
+            String textToSearch = "Grawe Carat";
+            Executor executor = new Executor("firefox");
+            executor.NavigateTo(url);
+            executor.InputText(textToSearch,"q");
+            IWebElement btn = executor.FindElementByName("btnK");
+            executor.ClickOnElement(btn);
+            //executor.CountResults(textToSearch);
+            executor.FindElementByXPath(".//*[contains(text(),'"+textToSearch+"')]");
+            Thread.Sleep(5000); //delay of 5000 milliseconds or 1 second.
+            executor.Close();
+        }
+    }
+}
